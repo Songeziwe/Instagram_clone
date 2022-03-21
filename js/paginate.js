e25fc7ec86a78303241e40a39845f0100966092b
Songeziwe Soboois <sobooissongeziwe@gmail.com>
2022-03-22 01:40:16 +0200

    About to create status pagination.

---
+++ js/paginate.js
@@ -0,0 +1,17 @@
+const items = [
+  '<p class="item">Item1</p>', '<p class="item">Item2</p>',
+  '<p class="item">Item3</p>', '<p class="item">Item4</p>',
+  '<p class="item">Item5</p>', '<p class="item">Item6</p>',
+  '<p class="item">Item7</p>', '<p class="item">Item8</p>',
+  '<p class="item">Item9</p>', '<p class="item">Item10</p>',
+  '<p class="item">Item11</p>', '<p class="item">Item12</p>',
+  '<p class="item">Item13</p>', '<p class="item">Item14</p>',
+  '<p class="item">Item15</p>', '<p class="item">Item16</p>',
+  '<p class="item">Item17</p>', '<p class="item">Item18</p>',
+  '<p class="item">Item19</p>', '<p class="item">Item20</p>',
+  '<p class="item">Item21</p>', '<p class="item">Item22</p>',
+  '<p class="item">Item23</p>', '<p class="item">Item24</p>',
+  '<p class="item">Item25</p>', '<p class="item">Item26</p>',
+  '<p class="item">Item27</p>', '<p class="item">Item28</p>',
+
+];
